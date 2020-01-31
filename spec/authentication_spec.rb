c3d4525bcd4939eb78e4384a87b27658cab2c457
Vashira <vashimgs@gmail.com>
2020-01-31 18:10:24 +0000

    Added authentication test and fixed the rubocop issues

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature 'Authentications', type: :feature do
   scenario 'Log in and log out' do
-    User.create(first_name: 'User', last_name: 'Name', email: 'example@example.com', password: 'word',
-                password_confirmation: 'word')
+    User.create!(first_name: 'User', last_name: 'Name', email: 'example@example.com', password: 'password',
+                 password_confirmation: 'password')
     visit new_user_session_path
     have_link 'Log in', href: new_user_session_path
     have_link 'Sign up', href: new_user_registration_path
     page.fill_in 'Email', with: 'example@example.com'
-    page.fill_in 'Password', with: 'word'
+    page.fill_in 'Password', with: 'password'
     click_button 'Log in'
     expect(page.current_path).to eq root_path
     have_link 'Users', href: users_path
