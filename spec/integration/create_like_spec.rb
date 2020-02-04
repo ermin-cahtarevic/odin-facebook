e97a6b7a73d8fd2e2989178873746fbde4510845
Vashira <vashimgs@gmail.com>
2020-02-04 20:29:48 +0000

    Added integration test for likes and fixed rubocop issues

--- spec/integration/create_like_spec.rb
+++ spec/integration/create_like_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-RSpec.feature 'Create Comment', type: :feature do
+RSpec.feature 'Create like', type: :feature do
   before :each do
     user = User.create!(first_name: 'User', last_name: 'Name', email: 'example@example.com', password: 'password',
                         password_confirmation: 'password')
-    post = user.posts.build(content: 'Test post')
-    post.save
+    @post = user.posts.build(content: 'Test post')
+    @post.save
     visit new_user_session_path
     page.fill_in 'Email', with: 'example@example.com'
     page.fill_in 'Password', with: 'password'
     click_button 'Log in'
   end
 
-  scenario 'Comment creation', type: :feature do
+  scenario 'Like creation', type: :feature do
     expect(page).to have_text 'Signed in successfully.'
-    page.fill_in 'Comment', with: 'Test comment'
-    click_button 'Comment'
-    expect(page).to have_text 'Comment added'
+    find(:css, 'i.fa-thumbs-up').click
+    expect(page.current_path).to eq root_path
   end
 end
