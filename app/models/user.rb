8e83f2963f3c8c07c6a4e8ea2af16c185bc39d11
Ermin Cahtarevic <erminc@windowslive.com>
2020-02-05 16:42:43 +0100

    Added friendship model and added a couple of methods in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,30 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :friendships, foreign_key: :sender_id
+  has_many :inverse_friendships, class_name: :Friendship, foreign_key: :reciever_id
+  
+  def friend_list
+    friend_list = friendships.map { |friendship| friendship.reciever if friendship.status }
+    friend_list + inverse_friendships.map { |friendship| friendship.sender if friendship.status }
+    friend_list.compact
+  end
+
+  def sent_requests
+    friendships.map { |friendship| friendship.reciever if !friendship.status }.compact
+  end
+
+  def recieved_requests
+    inverse_friendships.map { |friendship| friendship.sender if !friendship.status }.compact
+  end
+
+  def confirm_request(sender)
+    friendship = inverse_friendships.find { |friendship| friendship.sender == sender }
+    friendship.status = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friend_list.include?(user)
+  end
 end
