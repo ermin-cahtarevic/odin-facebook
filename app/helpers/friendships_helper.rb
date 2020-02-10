a480d12a201fdafd60d5cb7f0a475ed107261010
Vashira <vashimgs@gmail.com>
2020-02-10 15:35:23 +0000

    fixed rubocop issues

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,5 +1,4 @@
 module FriendshipsHelper
-
   def friend_list
     @friend_list = current_user.friendships.map { |friendship| friendship.reciever if friendship.status }
     @friend_list += current_user.inverse_friendships.map { |friendship| friendship.sender if friendship.status }
